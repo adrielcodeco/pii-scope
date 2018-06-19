53ed96dc709d7c8547e264baaff308d2fa68389d
Codeco <adrielcodeco@hotmail.com>
2018-06-19 10:23:08 -0300

    global variable resolved

--- test/index.unit.ts
+++ test/index.unit.ts
@@ -1,64 +1,64 @@
-(() => {
-  const testKey = 'Pii'
+export {}
 
-  const requireTest = () => {
-    // require scope
-    return require('../src').default
-  }
+const testKey = 'Pii'
 
-  const useNativeModule = () => {
-    const NativeModule = require('module')
-    return new NativeModule(module.filename, null)
-  }
+const requireTest = () => {
+  // require scope
+  return require('../src').default
+}
 
-  const getTestScoped = () => {
-    const Scope = requireTest()
-    // require newGlobalScope in new scope
-    return Scope.New(require.resolve('./dummy/newGlobalScope'), {
-      parentModule: useNativeModule()
-    })
-  }
+const useNativeModule = () => {
+  const NativeModule = require('module')
+  return new NativeModule(module.filename, null)
+}
 
-  test('require', () => {
-    expect.assertions(1)
-    const Scope = requireTest()
-    // check if scope have New static function
-    expect(Scope).toHaveProperty('New')
+const getTestScoped = () => {
+  const Scope = requireTest()
+  // require newGlobalScope in new scope
+  return Scope.New(require.resolve('./dummy/newGlobalScope'), {
+    parentModule: useNativeModule()
   })
+}
 
-  test('call New function', () => {
-    expect.assertions(3)
-    const NewGlobalScope = getTestScoped()
-    // check if then new scope is anything
-    expect(NewGlobalScope).toBeDefined()
-    expect(NewGlobalScope.prototype).toBeDefined()
-    const keys = Reflect.ownKeys(NewGlobalScope.prototype)
-    // check if the new scope have the properties getGlobal and setGlobal
-    expect(keys).toEqual(expect.arrayContaining(['getGlobal', 'setGlobal']))
-  })
+test('require', () => {
+  expect.assertions(1)
+  const Scope = requireTest()
+  // check if scope have New static function
+  expect(Scope).toHaveProperty('New')
+})
 
-  test('test scopes', () => {
-    expect.assertions(10)
-    const value = 'mainScope'
-    Reflect.set(global, testKey, value)
+test('call New function', () => {
+  expect.assertions(3)
+  const NewGlobalScope = getTestScoped()
+  // check if then new scope is anything
+  expect(NewGlobalScope).toBeDefined()
+  expect(NewGlobalScope.prototype).toBeDefined()
+  const keys = Reflect.ownKeys(NewGlobalScope.prototype)
+  // check if the new scope have the properties getGlobal and setGlobal
+  expect(keys).toEqual(expect.arrayContaining(['getGlobal', 'setGlobal']))
+})
 
-    const NewGlobalScope1 = getTestScoped()
-    const test1 = new NewGlobalScope1()
-    expect(test1.getGlobal(testKey)).not.toEqual(value)
-    test1.setGlobal(testKey, 'test1')
-    expect(test1.getGlobal(testKey)).toEqual('test1')
-    expect(Reflect.get(global, testKey)).not.toEqual('test1')
+test('test scopes', () => {
+  expect.assertions(10)
+  const value = 'mainScope'
+  Reflect.set(global, testKey, value)
 
-    const NewGlobalScope2 = getTestScoped()
-    const test2 = new NewGlobalScope2()
-    expect(test2.getGlobal(testKey)).not.toEqual(value)
-    expect(test2.getGlobal(testKey)).not.toEqual('test1')
-    test2.setGlobal(testKey, 'test2')
-    expect(test2.getGlobal(testKey)).toEqual('test2')
+  const NewGlobalScope1 = getTestScoped()
+  const test1 = new NewGlobalScope1()
+  expect(test1.getGlobal(testKey)).not.toEqual(value)
+  test1.setGlobal(testKey, 'test1')
+  expect(test1.getGlobal(testKey)).toEqual('test1')
+  expect(Reflect.get(global, testKey)).not.toEqual('test1')
 
-    expect(test1.getGlobal(testKey)).not.toEqual(value)
-    expect(test1.getGlobal(testKey)).not.toEqual('test2')
-    expect(test1.getGlobal(testKey)).toEqual('test1')
-    expect(Reflect.get(global, testKey)).not.toEqual('test2')
-  })
-})()
+  const NewGlobalScope2 = getTestScoped()
+  const test2 = new NewGlobalScope2()
+  expect(test2.getGlobal(testKey)).not.toEqual(value)
+  expect(test2.getGlobal(testKey)).not.toEqual('test1')
+  test2.setGlobal(testKey, 'test2')
+  expect(test2.getGlobal(testKey)).toEqual('test2')
+
+  expect(test1.getGlobal(testKey)).not.toEqual(value)
+  expect(test1.getGlobal(testKey)).not.toEqual('test2')
+  expect(test1.getGlobal(testKey)).toEqual('test1')
+  expect(Reflect.get(global, testKey)).not.toEqual('test2')
+})
--- test/scope.unit.ts
+++ test/scope.unit.ts
@@ -1,60 +1,60 @@
-(() => {
-  const requireTest = () => {
-    // require scope
-    return require('../src/scope').default
-  }
+export {}
 
-  const useNativeModule = () => {
-    const NativeModule = require('module')
-    return new NativeModule(module.filename, null)
-  }
+const requireTest = () => {
+  // require scope
+  return require('../src/scope').default
+}
 
-  test('require', () => {
-    expect.assertions(3)
-    let Scope
-    expect(() => {
-      Scope = requireTest()
-    }).not.toThrow()
-    expect(Scope).toBeDefined()
-    // tslint:disable-next-line: strict-type-predicates
-    expect(typeof Scope === 'function').toBeTruthy()
-  })
+const useNativeModule = () => {
+  const NativeModule = require('module')
+  return new NativeModule(module.filename, null)
+}
 
-  test('validation of invalid path', () => {
-    expect.assertions(1)
-    const Scope = requireTest()
-    expect(() => {
-      Scope()
-    }).toThrowError(/the path argument is invalid/)
-  })
+test('require', () => {
+  expect.assertions(3)
+  let Scope
+  expect(() => {
+    Scope = requireTest()
+  }).not.toThrow()
+  expect(Scope).toBeDefined()
+  // tslint:disable-next-line: strict-type-predicates
+  expect(typeof Scope === 'function').toBeTruthy()
+})
 
-  test('validation of absolute path', () => {
-    expect.assertions(1)
-    const Scope = requireTest()
-    expect(() => {
-      Scope('./dummy/newGlobalScope')
-    }).toThrowError(/the path argument is not an absolute path/)
-  })
+test('validation of invalid path', () => {
+  expect.assertions(1)
+  const Scope = requireTest()
+  expect(() => {
+    Scope()
+  }).toThrowError(/the path argument is invalid/)
+})
 
-  test('validation of arguments', () => {
-    expect.assertions(1)
-    const Scope = requireTest()
-    expect(() => {
-      Scope(require.resolve('./dummy/newGlobalScope'), {
-        parentModule: useNativeModule()
-      })
-    }).not.toThrow()
-  })
+test('validation of absolute path', () => {
+  expect.assertions(1)
+  const Scope = requireTest()
+  expect(() => {
+    Scope('./dummy/newGlobalScope')
+  }).toThrowError(/the path argument is not an absolute path/)
+})
 
-  test('using noCacheFor', () => {
-    expect.assertions(1)
-    const Scope = requireTest()
-    expect(() => {
-      const filename = require.resolve('./dummy/newGlobalScope')
-      Scope(filename, {
-        noCacheFor: [filename, './dummy/getGlobalVar'],
-        parentModule: useNativeModule()
-      })
-    }).not.toThrow()
-  })
-})()
+test('validation of arguments', () => {
+  expect.assertions(1)
+  const Scope = requireTest()
+  expect(() => {
+    Scope(require.resolve('./dummy/newGlobalScope'), {
+      parentModule: useNativeModule()
+    })
+  }).not.toThrow()
+})
+
+test('using noCacheFor', () => {
+  expect.assertions(1)
+  const Scope = requireTest()
+  expect(() => {
+    const filename = require.resolve('./dummy/newGlobalScope')
+    Scope(filename, {
+      noCacheFor: [filename, './dummy/getGlobalVar'],
+      parentModule: useNativeModule()
+    })
+  }).not.toThrow()
+})
